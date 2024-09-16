c4f330d4152e7e7544f461c075c7eb72f2cd0215
Sourabh Singh <65441264+so5tech@users.noreply.github.com>
2024-09-16 18:21:33 +0530

    Delete dist directory

--- dist/__tests__/handlers/users.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { User } from "../../controllers/BO/user"
-Object.defineProperty(exports, "__esModule", { value: true });
-// describe('getUsers', () => {
-//     it("should return an array of users", () => {
-//         User()
-//     })
-// })
-//# sourceMappingURL=users.test.js.map
